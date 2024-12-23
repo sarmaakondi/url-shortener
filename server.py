2473b2d1e81306c88cdbaa5f6c433fd30dc49dc5
sarmaakondi <avnmsarma@gmail.com>
2024-12-24 10:31:50 +1100

    Create class based URL shortener

--- server.py
+++ server.py
@@ -1,20 +1,33 @@
-from flask import Flask, request, redirect
+import random
+import string
+from flask import Flask, request, jsonify, redirect
 
 app = Flask(__name__)
 
-next_url_path = 0
-url_mappings = {}
 
+class URLShortener:
+    def __init__(self, shortened_length=8):
+        self.shortened_length = shortened_length
+        self.url_mappings = {}
 
-@app.post("/shorten")
-def create_new_short_url():
-    global next_url_path
-    long_url = request.json["url"]
-    short_url = f"/s/{next_url_path}"
-    next_url_path += 1
-    url_mappings[short_url] = long_url
+    def generate_short_url(self):
+        """Generate a unique short URL."""
+        while True:
+            short_url = "".join(
+                random.choices(string.ascii_lowercase, k=self.shortened_length)
+            )
+            if short_url not in self.url_mappings:
+                return short_url
 
-    return short_url
+    def add_mapping(self, long_url):
+        """Creates a mapping from a long URL to a short URL."""
+        short_url = self.generate_short_url()
+        self.url_mappings[short_url] = long_url
+        return short_url
+
+    def get_long_url(self, short_url):
+        """Retrieves the long URL associated with a short URL."""
+        return self.url_mappings.get(short_url)
 
 
 if __name__ == "__main__":
